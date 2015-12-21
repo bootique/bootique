1dd74833af22f45928205f25bbdbc2151eb1d759
Andrus Adamchik <andrus@objectstyle.com>
2015-12-21 16:12:03 +0300

    Service/Module override support #6
    proper itests

--- src/test/java/com/nhl/bootique/BootiqueIT.java
+++ src/test/java/com/nhl/bootique/BootiqueIT.java
@@ -2,55 +2,21 @@ package com.nhl.bootique;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
-import static org.mockito.Mockito.mock;
 
-import java.util.Arrays;
 import java.util.Collection;
-import java.util.Collections;
 
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 
-import com.google.inject.Module;
-import com.nhl.bootique.it.ItestModule;
 import com.nhl.bootique.it.ItestModuleProvider;
 
 public class BootiqueIT {
 
-	@Before
-	public void before() {
-		ItestModule.MOCK_DELEGATE = mock(Module.class);
-	}
-
-	@After
-	public void after() {
-		ItestModuleProvider.REPLACES = null;
-		ItestModule.MOCK_DELEGATE = null;
-	}
-
 	@Test
-	public void testCreateAutoLoadModules() {
-		Collection<Module> autoLoaded = Bootique.app(new String[0]).autoLoadModules()
-				.createAutoLoadModules(Collections.emptyList());
+	public void testAutoLoadedProviders() {
+		Collection<BQModuleProvider> autoLoaded = Bootique.app(new String[0]).autoLoadedProviders();
 
 		assertEquals(1, autoLoaded.size());
-		autoLoaded.forEach(m -> assertTrue(m instanceof ItestModule));
+		autoLoaded.forEach(m -> assertTrue(m instanceof ItestModuleProvider));
 	}
 
-	@Test
-	public void testCreateAutoLoadModules_ExplicitIgnored() {
-
-		Collection<Module> existing = Arrays.asList(new ItestModule());
-		assertEquals(0, Bootique.app(new String[0]).autoLoadModules().createAutoLoadModules(existing).size());
-	}
-
-	@Test
-	public void testCreateAutoLoadModules_ReplaceAnotherModule() {
-
-		ItestModuleProvider.REPLACES = BQCoreModule.class;
-
-		Collection<Module> existing = Arrays.asList(new ItestModule());
-		assertEquals(0, Bootique.app(new String[0]).autoLoadModules().createAutoLoadModules(existing).size());
-	}
 }
--- src/test/java/com/nhl/bootique/it/ItestModule.java
+++ src/test/java/com/nhl/bootique/it/ItestModule.java
@@ -5,13 +5,9 @@ import com.google.inject.Module;
 
 public class ItestModule implements Module {
 
-	public static Module MOCK_DELEGATE;
-
 	@Override
 	public void configure(Binder binder) {
-		if (MOCK_DELEGATE != null) {
-			MOCK_DELEGATE.configure(binder);
-		}
+		// do nothing for now
 	}
 
 }
