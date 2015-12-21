48296f8032e849d01a80741a52d7368580c79c07
Andrus Adamchik <andrus@objectstyle.com>
2015-12-21 10:25:30 +0300

    unit test name mismatch

--- src/test/java/com/nhl/bootique/BQBinderTest.java
+++ src/test/java/com/nhl/bootique/BQBinderTest.java
@@ -11,7 +11,7 @@ import com.google.inject.Inject;
 import com.google.inject.Injector;
 import com.nhl.bootique.env.EnvironmentProperties;
 
-public class BQContribBinderTest {
+public class BQBinderTest {
 
 	@Test
 	public void testBindProperty() {
