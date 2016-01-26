46cc8e11fc8f2221e27e505696bd059168b15c99
Andrus Adamchik <andrus@objectstyle.com>
2016-01-26 10:51:50 +0300

    javadoc

--- src/main/java/com/nhl/bootique/command/Command.java
+++ src/main/java/com/nhl/bootique/command/Command.java
@@ -13,11 +13,18 @@ public interface Command {
 	 * @param options
 	 *            command-line options object.
 	 * @return CommandOutcome object that indicates to the caller whether
-	 *         command was successful and whether it needs to continue with
-	 *         command chain.
+	 *         command was successful and whether the caller needs to continue
+	 *         with command chain.
 	 */
 	CommandOutcome run(Options options);
 
+	/**
+	 * Allows subclasses to configure visible CLI options.
+	 * 
+	 * @param parser
+	 *            a mutable CLI parser that stores supported command line
+	 *            options.
+	 */
 	default void configOptions(OptionParser parser) {
 		// do nothing by default...
 	}
--- src/main/java/com/nhl/bootique/command/FailoverHelpCommand.java
+++ src/main/java/com/nhl/bootique/command/FailoverHelpCommand.java
@@ -4,12 +4,16 @@ import com.google.inject.Inject;
 import com.nhl.bootique.jopt.Options;
 import com.nhl.bootique.log.BootLogger;
 
+/**
+ * Prints help regardless of whether the help option was requested or not.
+ * Intended to be used as a failover command when no other command was
+ * explicitly specified.
+ */
 public class FailoverHelpCommand extends HelpCommand {
 
 	@Inject
 	public FailoverHelpCommand(BootLogger bootLogger) {
 		super(bootLogger);
-		// TODO Auto-generated constructor stub
 	}
 
 	@Override
