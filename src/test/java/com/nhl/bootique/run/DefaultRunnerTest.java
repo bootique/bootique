3597f5d00d6e66f364836605e5a1a409160eb7c7
Andrus Adamchik <andrus@objectstyle.com>
2016-02-01 09:00:49 +0100

    unused import

--- src/test/java/com/nhl/bootique/run/DefaultRunnerTest.java
+++ src/test/java/com/nhl/bootique/run/DefaultRunnerTest.java
@@ -7,7 +7,6 @@ import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.verifyZeroInteractions;
 import static org.mockito.Mockito.when;
 
 import java.util.Arrays;
