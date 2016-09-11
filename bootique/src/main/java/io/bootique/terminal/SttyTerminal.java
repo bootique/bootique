afdd12e4f8c252a8b41af22b42d1fd3ecdf5e31b
Andrus Adamchik <andrus@objectstyle.com>
2016-09-11 08:30:52 -0400

    Support for terminal width detection #95
    * unit tests
    * second pattern for Linux

--- bootique/src/main/java/io/bootique/terminal/SttyTerminal.java
+++ bootique/src/main/java/io/bootique/terminal/SttyTerminal.java
@@ -8,15 +8,18 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
- * A terminal based on UNIX 'tput' command.
+ * A terminal based on UNIX 'stty -a' command.
  *
  * @since 0.20
  */
 public class SttyTerminal extends ExternalCommandTerminal {
 
-    // TODO: is "columns" a localizable word?
-    private static final Pattern COLUMNS_PATTERN = Pattern.compile("([0-9]+)\\scolumns;");
+    // OSX:     speed 9600 baud; 43 rows; 141 columns;
+    // Linux:   speed 9600 baud; rows 40; columns 148; line = 0;
+    private static final Pattern COLUMNS_PATTERN_OSX = Pattern.compile("([0-9]+)\\scolumns;");
+    private static final Pattern COLUMNS_PATTERN_LINUX = Pattern.compile("columns\\s([0-9]+);");
 
+    // "stty" seems to be present in /bin on OSX, CentOS, Ubuntu...
     private static final String[] STTY_COMMAND = new String[]{"/bin/stty", "-a"};
 
     public SttyTerminal(Terminal failoverTerminal, BootLogger logger) {
@@ -32,10 +35,7 @@ public class SttyTerminal extends ExternalCommandTerminal {
     protected Integer parseColumns(BufferedReader commandOutput) {
 
         //
-        // on OSX...
-        // columns information is printed on line 1 and looks like this:
-        //
-        // speed 9600 baud; 43 rows; 141 columns;
+        // columns information is printed on line 1, so ignoring the rest of it...
         //
 
         String line1;
@@ -49,25 +49,38 @@ public class SttyTerminal extends ExternalCommandTerminal {
             return null;
         }
 
-        Matcher matcher = COLUMNS_PATTERN.matcher(line1);
+        return parseLine(line1);
+    }
 
-        if (!matcher.find()) {
-            logger.trace(() ->
-                    "Unexpected output for '"
-                            + ExternalCommandTerminal.toString(getColumnsCommand())
-                            + "' command: "
-                            + line1);
-            return null;
+    protected Integer parseLine(String line) {
+        return parseLine(line, COLUMNS_PATTERN_LINUX, COLUMNS_PATTERN_OSX);
+    }
+
+    protected Integer parseLine(String line, Pattern... patterns) {
+        Matcher matcher;
+
+        for (Pattern pattern : patterns) {
+            matcher = pattern.matcher(line);
+
+            if (matcher.find()) {
+                try {
+                    return Integer.parseInt(matcher.group(1));
+                } catch (NumberFormatException e) {
+                    logger.trace(() ->
+                            "Unexpected output for '"
+                                    + ExternalCommandTerminal.toString(getColumnsCommand())
+                                    + "' command: "
+                                    + line);
+                    return null;
+                }
+            }
         }
-        try {
-            return Integer.parseInt(matcher.group(1));
-        } catch (NumberFormatException e) {
-            logger.trace(() ->
-                    "Unexpected output for '"
-                            + ExternalCommandTerminal.toString(getColumnsCommand())
-                            + "' command: "
-                            + line1);
-            return null;
-        }
+
+        logger.trace(() ->
+                "Unexpected output for '"
+                        + ExternalCommandTerminal.toString(getColumnsCommand())
+                        + "' command: "
+                        + line);
+        return null;
     }
 }
---
+++ bootique/src/test/java/io/bootique/terminal/SttyTerminalTest.java
@@ -0,0 +1,51 @@
+package io.bootique.terminal;
+
+import io.bootique.log.BootLogger;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+import static org.mockito.Mockito.mock;
+
+public class SttyTerminalTest {
+
+    private Terminal mockFailover;
+    private SttyTerminal terminal;
+
+    @Before
+    public void before() {
+        mockFailover = mock(Terminal.class);
+        terminal = new SttyTerminal(mockFailover, mock(BootLogger.class));
+    }
+
+    @Test
+    public void testParseLine_OSX() {
+        String line = "speed 9600 baud; 39 rows; 136 columns;";
+        assertEquals(new Integer(136), terminal.parseLine(line));
+    }
+
+    @Test
+    public void testParseLine_Linux() {
+        // from Centos 7:
+        String line = "speed 9600 baud; rows 40; columns 148; line = 0;";
+        assertEquals(new Integer(148), terminal.parseLine(line));
+    }
+
+    @Test
+    public void testParseLine_Docker_Ubuntu() {
+
+        // docker  run -t ubuntu:latest stty -a
+        // same as any lunux, except the value is 0
+
+        String line = "speed 38400 baud; rows 0; columns 0; line = 0;";
+        assertEquals(new Integer(0), terminal.parseLine(line));
+    }
+
+    @Test
+    public void testParseLine_Invalid() {
+
+        String line = "not a valid line";
+        assertNull(terminal.parseLine(line));
+    }
+}
