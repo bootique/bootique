4809e2c9e6449a8d9a2b5dcaa636d6948d51b493
Andrus Adamchik <andrus@objectstyle.com>
2016-09-12 08:58:27 -0400

    Standalone service for printing help #92
    * documenting package purpose

---
+++ bootique/src/main/java/io/bootique/application/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Package contains Bootique application metadata classes that describe a model of the application, its supported
+ * commands and options. All in a format independent from the chosen command-line style.
+ */
+package io.bootique.application;
