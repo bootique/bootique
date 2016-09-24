17188bf6016ff3e8515a83a83ad87c44388bb605
Andrus Adamchik <andrus@objectstyle.com>
2016-09-24 18:39:07 +0300

    cleanup

--- bootique/src/test/java/io/bootique/resource/ResourceFactory_WebConfigSourceIT.java
+++ bootique/src/test/java/io/bootique/resource/ResourceFactory_WebConfigSourceIT.java
@@ -1,14 +1,5 @@
 package io.bootique.resource;
 
-import static org.junit.Assert.assertEquals;
-
-import java.io.IOException;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.ServerConnector;
 import org.eclipse.jetty.servlet.ServletContextHandler;
@@ -17,50 +8,53 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+
 public class ResourceFactory_WebConfigSourceIT {
 
-	private Server jetty;
+    private Server jetty;
 
-	@Before
-	public void before() throws Exception {
-		startServer();
-	}
+    @After
+    public void after() throws Exception {
+        jetty.stop();
+    }
 
-	@After
-	public void after() throws Exception {
-		jetty.stop();
-	}
+    @Before
+    public void before() throws Exception {
 
-	private void startServer() throws Exception {
-		this.jetty = new Server();
+        this.jetty = new Server();
 
-		ServerConnector connector = new ServerConnector(this.jetty);
-		connector.setPort(12025);
-		jetty.addConnector(connector);
+        ServerConnector connector = new ServerConnector(this.jetty);
+        connector.setPort(12025);
+        jetty.addConnector(connector);
 
-		ServletContextHandler handler = new ServletContextHandler();
-		handler.setContextPath("/");
+        ServletContextHandler handler = new ServletContextHandler();
+        handler.setContextPath("/");
 
-		ServletHolder holder = new ServletHolder(new ConfigServlet());
-		handler.addServlet(holder, "/*");
-		jetty.setHandler(handler);
+        ServletHolder holder = new ServletHolder(new ConfigServlet());
+        handler.addServlet(holder, "/*");
+        jetty.setHandler(handler);
+        jetty.start();
+    }
 
-		jetty.start();
-	}
+    @Test
+    public void testReadConfig_HttpUrl() throws IOException {
+        String url = "http://localhost:12025/";
+        assertEquals("g: h", ResourceFactoryTest.resourceContents(url));
+    }
 
-	@Test
-	public void testReadConfig_HttpUrl() throws IOException {
-		String url = "http://localhost:12025/";
-		assertEquals("g: h", ResourceFactoryTest.resourceContents(url));
-	}
+    static class ConfigServlet extends HttpServlet {
+        private static final long serialVersionUID = -5746986231054267492L;
 
-	static class ConfigServlet extends HttpServlet {
-		private static final long serialVersionUID = -5746986231054267492L;
-
-		@Override
-		protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-			resp.getWriter().write("g: h");
-		}
-	}
-
+        @Override
+        protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+            resp.getWriter().write("g: h");
+        }
+    }
 }
