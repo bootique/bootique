def3c73e08c110b8b24e6711c6faa2dc9f5f3a1c
Andrus Adamchik <andrus@objectstyle.com>
2016-12-27 10:00:57 +0300

    HelpConfigCommand: Help for configs #27
    * Support for list properties in compiler

--- bootique/src/main/java/io/bootique/DeferredModuleMetadataSupplier.java
+++ bootique/src/main/java/io/bootique/DeferredModuleMetadataSupplier.java
@@ -2,11 +2,14 @@ package io.bootique;
 
 import io.bootique.annotation.BQConfig;
 import io.bootique.annotation.BQConfigProperty;
+import io.bootique.module.ConfigListMetadata;
 import io.bootique.module.ConfigObjectMetadata;
 import io.bootique.module.ConfigPropertyMetadata;
 import io.bootique.module.ModuleMetadata;
 
 import java.lang.reflect.Method;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
@@ -23,6 +26,113 @@ class DeferredModuleMetadataSupplier implements Supplier<Collection<ModuleMetada
     private static final Pattern SETTER = Pattern.compile("^set([A-Z].*)$");
 
     private Collection<BQModule> modules;
+
+    static ConfigObjectMetadata toConfig(String name, Class<?> type) {
+
+        ConfigObjectMetadata.Builder builder = ConfigObjectMetadata.builder(name).type(type);
+
+        // note that root config object known to Bootique doesn't require BQConfig annotation (though it would help in
+        // determining description). Objects nested within the root config do. Otherwise they will be treated as
+        // "simple" properties.
+        BQConfig configAnnotation = type.getAnnotation(BQConfig.class);
+        if (configAnnotation != null) {
+            builder.description(configAnnotation.value());
+        }
+
+        for (Method m : type.getMethods()) {
+            BQConfigProperty configPropertyAnnotation = m.getAnnotation(BQConfigProperty.class);
+            if (configPropertyAnnotation != null) {
+                builder.addProperty(compile(propertyTypeFromSetter(m), propertyNameFromSetter(m), configPropertyAnnotation));
+            }
+        }
+
+        return builder.build();
+    }
+
+    private static ConfigPropertyMetadata compile(Type type, String name, BQConfigProperty propertyAnnotation) {
+
+        Class<?> typeClass = Object.class;
+        Class<?> parameterType = Object.class;
+
+        if (type instanceof Class) {
+            typeClass = (Class<?>) type;
+        } else if (type instanceof ParameterizedType) {
+            ParameterizedType parameterizedType = (ParameterizedType) type;
+
+            if (parameterizedType.getRawType() instanceof Class) {
+                typeClass = (Class<?>) parameterizedType.getRawType();
+            }
+
+            Type[] args = parameterizedType.getActualTypeArguments();
+            if (args.length == 1 && args[0] instanceof Class) {
+                parameterType = (Class<?>) args[0];
+            }
+        }
+
+        if (Collection.class.isAssignableFrom(typeClass)) {
+            return toCollectionProperty(name, typeClass, parameterType, propertyAnnotation);
+        } else if (typeClass.isAnnotationPresent(BQConfig.class)) {
+            return toConfig(name, typeClass);
+        } else {
+            return toConfigProperty(name, typeClass, propertyAnnotation);
+        }
+    }
+
+    private static ConfigListMetadata toCollectionProperty(
+            String name,
+            Class<?> collectionType,
+            Class<?> elementType,
+            BQConfigProperty annotation) {
+
+        ConfigPropertyMetadata elementMetadata = ConfigPropertyMetadata.builder().type(elementType).build();
+
+        return ConfigListMetadata
+                .builder(name)
+                .type(collectionType)
+                .description(annotation.value())
+                .elementType(elementMetadata)
+                .build();
+    }
+
+    private static ConfigPropertyMetadata toConfigProperty(String name, Class<?> type, BQConfigProperty annotation) {
+        return ConfigPropertyMetadata
+                .builder(name)
+                .type(type)
+                .description(annotation.value())
+                .build();
+    }
+
+    private static Type propertyTypeFromSetter(Method maybeSetter) {
+
+        if (!Void.TYPE.equals(maybeSetter.getReturnType())) {
+            throw new IllegalStateException("Method '" + maybeSetter.getDeclaringClass().getName() + "."
+                    + maybeSetter.getName() +
+                    "' is annotated with @BQConfigProperty, but does not match expected setter signature."
+                    + " It must be void.");
+        }
+
+        Type[] paramTypes = maybeSetter.getGenericParameterTypes();
+        if (paramTypes.length != 1) {
+            throw new IllegalStateException("Method '" + maybeSetter.getDeclaringClass().getName() + "."
+                    + maybeSetter.getName() +
+                    "' is annotated with @BQConfigProperty, but does not match expected setter signature."
+                    + " It must take exactly one parameter");
+        }
+
+        return paramTypes[0];
+    }
+
+    private static String propertyNameFromSetter(Method maybeSetter) {
+        Matcher matcher = SETTER.matcher(maybeSetter.getName());
+        if (!matcher.find()) {
+            throw new IllegalStateException("Method '" + maybeSetter.getDeclaringClass().getName() + "."
+                    + maybeSetter.getName() +
+                    "' is annotated with @BQConfigProperty, but method name is not a JavaBean setter.");
+        }
+
+        String raw = matcher.group(1);
+        return Character.toLowerCase(raw.charAt(0)) + raw.substring(1);
+    }
 
     @Override
     public Collection<ModuleMetadata> get() {
@@ -68,74 +178,8 @@ class DeferredModuleMetadataSupplier implements Supplier<Collection<ModuleMetada
         return configs;
     }
 
-    private ConfigObjectMetadata toConfig(String name, Class<?> type) {
+    private enum PropertyType {
 
-        ConfigObjectMetadata.Builder builder = ConfigObjectMetadata.builder(name).type(type);
-
-        // note that root config object known to Bootique doesn't require BQConfig annotation (though it would help in
-        // determining description). Objects nested within the root config do. Otherwise they will be treated as
-        // "simple" properties.
-        BQConfig configAnnotation = type.getAnnotation(BQConfig.class);
-        if (configAnnotation != null) {
-            builder.description(configAnnotation.value());
-        }
-
-        for (Method m : type.getMethods()) {
-            BQConfigProperty configProperty = m.getAnnotation(BQConfigProperty.class);
-            if (configProperty != null) {
-
-                Class<?> propertyType = propertyTypeFromSetter(m);
-                String propertyName = propertyNameFromSetter(m);
-
-                // now check if the object is itself a nested config...
-                ConfigPropertyMetadata propertyMetadata = propertyType.isAnnotationPresent(BQConfig.class)
-                        ? toConfig(propertyName, propertyType)
-                        : toConfigProperty(propertyName, propertyType, configProperty);
-
-                builder.addProperty(propertyMetadata);
-            }
-        }
-
-        return builder.build();
-    }
-
-    private ConfigPropertyMetadata toConfigProperty(String name, Class<?> type, BQConfigProperty annotation) {
-        return ConfigPropertyMetadata
-                .builder(name)
-                .type(type)
-                .description(annotation.value())
-                .build();
-    }
-
-    private Class<?> propertyTypeFromSetter(Method maybeSetter) {
-
-        if (!Void.TYPE.equals(maybeSetter.getReturnType())) {
-            throw new IllegalStateException("Method '" + maybeSetter.getDeclaringClass().getName() + "."
-                    + maybeSetter.getName() +
-                    "' is annotated with @BQConfigProperty, but does not match expected setter signature."
-                    + " It must be void.");
-        }
-
-        Class<?>[] paramTypes = maybeSetter.getParameterTypes();
-        if (paramTypes.length != 1) {
-            throw new IllegalStateException("Method '" + maybeSetter.getDeclaringClass().getName() + "."
-                    + maybeSetter.getName() +
-                    "' is annotated with @BQConfigProperty, but does not match expected setter signature."
-                    + " It must take exactly one parameter");
-        }
-
-        return paramTypes[0];
-    }
-
-    private String propertyNameFromSetter(Method maybeSetter) {
-        Matcher matcher = SETTER.matcher(maybeSetter.getName());
-        if (!matcher.find()) {
-            throw new IllegalStateException("Method '" + maybeSetter.getDeclaringClass().getName() + "."
-                    + maybeSetter.getName() +
-                    "' is annotated with @BQConfigProperty, but method name is not a JavaBean setter.");
-        }
-
-        String raw = matcher.group(1);
-        return Character.toLowerCase(raw.charAt(0)) + raw.substring(1);
+        scalar, object, collection
     }
 }
---
+++ bootique/src/test/java/io/bootique/DeferredModuleMetadataSupplierTest.java
@@ -0,0 +1,92 @@
+package io.bootique;
+
+import io.bootique.annotation.BQConfig;
+import io.bootique.annotation.BQConfigProperty;
+import io.bootique.module.ConfigListMetadata;
+import io.bootique.module.ConfigObjectMetadata;
+import io.bootique.module.ConfigPropertyMetadata;
+import org.junit.Test;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+
+public class DeferredModuleMetadataSupplierTest {
+
+    @Test
+    public void testToConfig() {
+
+        ConfigObjectMetadata md = DeferredModuleMetadataSupplier.toConfig("prefix", Config1.class);
+        assertNotNull(md);
+
+        assertEquals("prefix", md.getName());
+        assertEquals("Describes Config1", md.getDescription());
+        assertEquals(Config1.class, md.getType());
+
+        assertEquals(4, md.getProperties().size());
+
+        Map<String, ConfigPropertyMetadata> propMap = md
+                .getProperties()
+                .stream()
+                .collect(Collectors.toMap(ConfigPropertyMetadata::getName, Function.identity()));
+
+        ConfigPropertyMetadata p1 = propMap.get("p1");
+        assertNotNull(p1);
+        assertEquals(Integer.TYPE, p1.getType());
+        assertNull(p1.getDescription());
+
+        ConfigPropertyMetadata p2 = propMap.get("p2");
+        assertNotNull(p2);
+        assertEquals(BigDecimal.class, p2.getType());
+        assertEquals("description of p2", p2.getDescription());
+
+        ConfigObjectMetadata p3 = (ConfigObjectMetadata) propMap.get("p3");
+        assertNotNull(p3);
+        assertEquals(Config2.class, p3.getType());
+        assertEquals(1, p3.getProperties().size());
+
+        ConfigListMetadata p4 = (ConfigListMetadata) propMap.get("p4");
+        assertNotNull(p4);
+        assertEquals(List.class, p4.getType());
+        assertNotNull(p4.getElementType());
+        assertEquals(String.class, p4.getElementType().getType());
+
+    }
+
+    @BQConfig("Describes Config1")
+    public static class Config1 {
+
+        @BQConfigProperty
+        public void setP1(int v) {
+        }
+
+        @BQConfigProperty("description of p2")
+        public void setP2(BigDecimal v) {
+        }
+
+        @BQConfigProperty
+        public void setP3(Config2 v) {
+        }
+
+        @BQConfigProperty
+        public void setP4(List<String> v) {
+        }
+
+        public void setNonP(String v) {
+        }
+    }
+
+    @BQConfig("Describes Config2")
+    public static class Config2 {
+
+        @BQConfigProperty
+        public void setP1(byte v) {
+        }
+    }
+}
