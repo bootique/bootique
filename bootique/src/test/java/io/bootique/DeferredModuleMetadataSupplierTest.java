5e7e095c6f0f5f9e3fbf88ebd0244dbc6a918686
Andrus Adamchik <andrus@objectstyle.com>
2016-12-28 14:16:26 +0300

    HelpConfigCommand: Help for configs #27
    * support for map configs - addressing metadata cycles

--- bootique/src/test/java/io/bootique/DeferredModuleMetadataSupplierTest.java
+++ bootique/src/test/java/io/bootique/DeferredModuleMetadataSupplierTest.java
@@ -9,6 +9,7 @@ import io.bootique.meta.config.ConfigValueMetadata;
 import org.junit.Test;
 
 import java.math.BigDecimal;
+import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
@@ -38,12 +39,10 @@ public class DeferredModuleMetadataSupplierTest {
                 .collect(Collectors.toMap(ConfigValueMetadata::getName, Function.identity()));
 
         ConfigValueMetadata p1 = propMap.get("p1");
-        assertNotNull(p1);
         assertEquals(Integer.TYPE, p1.getType());
         assertNull(p1.getDescription());
 
         ConfigValueMetadata p2 = propMap.get("p2");
-        assertNotNull(p2);
         assertEquals(BigDecimal.class, p2.getType());
         assertEquals("description of p2", p2.getDescription());
 
@@ -72,6 +71,23 @@ public class DeferredModuleMetadataSupplierTest {
         assertEquals(Integer.class, p7.getKeysType());
         assertEquals(Config2.class, p7.getValuesType().getType());
         assertEquals(ConfigObjectMetadata.class, p7.getValuesType().getClass());
+    }
+
+    @Test
+    public void testToConfig_Cycle() {
+
+        ConfigObjectMetadata md = DeferredModuleMetadataSupplier.toConfig("prefix", Config3.class);
+        assertNotNull(md);
+
+        assertEquals("prefix", md.getName());
+        assertEquals(Config3.class, md.getType());
+
+        assertEquals(1, md.getProperties().size());
+
+        ConfigListMetadata p1 = (ConfigListMetadata) md.getProperties().iterator().next();
+        assertEquals(List.class, p1.getType());
+        assertEquals(Config4.class, p1.getElementType().getType());
+        assertEquals(ConfigObjectMetadata.class, p1.getElementType().getClass());
     }
 
     @BQConfig("Describes Config1")
@@ -114,6 +130,22 @@ public class DeferredModuleMetadataSupplierTest {
 
         @BQConfigProperty
         public void setP1(byte v) {
+        }
+    }
+
+    @BQConfig
+    public static class Config3 {
+
+        @BQConfigProperty
+        public void setC4s(Collection<Config4> v) {
+        }
+    }
+
+    @BQConfig
+    public static class Config4 {
+
+        @BQConfigProperty
+        public void setC3(Config3 v) {
         }
     }
 }
