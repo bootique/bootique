bootique/bootique

commit d26a05bd458500562de58c792d249811acdc00da

removing unused class

bootique/src/main/java/io/bootique/cli/CliParser.java
@@ -1,9 +1,0 @@
-package io.bootique.cli;
-
-/**
- * @since 0.20
- */
-public class CliParser {
-
-
-}

commit 795f6347ba4ce3052140af7d58ecdd9c3440483c

Delete CliParser.java
Initially removed via d26a05bd458500562de58c792d249811acdc00da, inadvertently added back via a087afdcc267a3dc3895008b807a467c481ee78f

bootique/src/main/java/io/bootique/cli/CliParser.java
@@ -1,9 +1,0 @@
-package io.bootique.cli;
-
-/**
- * @since 0.20
- */
-public class CliParser {
-
-
-}