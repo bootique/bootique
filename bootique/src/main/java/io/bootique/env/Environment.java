8012f99bb12e6924ca4f38f10a364e924737464b
Andrus Adamchik <andrus@objectstyle.com>
2017-08-31 10:13:31 +0300

    Deprecating BQ_* vars #175

--- bootique/src/main/java/io/bootique/env/Environment.java
+++ bootique/src/main/java/io/bootique/env/Environment.java
@@ -8,66 +8,67 @@ import java.util.Map;
  */
 public interface Environment {
 
-	String FRAMEWORK_PROPERTIES_PREFIX = "bq";
-	
-	/**
-	 * @since 0.17
-	 */
-	String FRAMEWORK_VARIABLES_PREFIX = "BQ_";
+    String FRAMEWORK_PROPERTIES_PREFIX = "bq";
 
-	String getProperty(String name);
+    /**
+     * @since 0.17
+     * @deprecated since 0.24 as BQ_ variables are deprecated.
+     */
+    @Deprecated
+    String FRAMEWORK_VARIABLES_PREFIX = "BQ_";
 
-	/**
-	 * Returns all properties in this environment that start with a given prefix
-	 * plus a dot separator. The prefix is stripped from the property name in
-	 * the Map.
-	 * 
-	 * @param prefix
-	 *            a prefix to qualify properties with.
-	 * @return all properties in this environment that start with a given prefix
-	 *         plus a dot separator.
-	 */
-	Map<String, String> subproperties(String prefix);
+    String getProperty(String name);
 
-	/**
-	 * An equivalent to calling {@link #subproperties(String)} with "bq" prefix
-	 * argument.
-	 * 
-	 * @return a map of all properties that start with "bq." prefix.
-	 */
-	default Map<String, String> frameworkProperties() {
-		return subproperties(FRAMEWORK_PROPERTIES_PREFIX);
-	}
+    /**
+     * Returns all properties in this environment that start with a given prefix
+     * plus a dot separator. The prefix is stripped from the property name in
+     * the Map.
+     *
+     * @param prefix a prefix to qualify properties with.
+     * @return all properties in this environment that start with a given prefix
+     * plus a dot separator.
+     */
+    Map<String, String> subproperties(String prefix);
 
-	/**
-	 * Returns a value of the environment variable with a given name.
-	 * 
-	 * @since 0.17
-	 * @param name
-	 *            environment variable name.
-	 * @return a value of the environment variable with a given name.
-	 */
-	String getVariable(String name);
+    /**
+     * An equivalent to calling {@link #subproperties(String)} with "bq" prefix
+     * argument.
+     *
+     * @return a map of all properties that start with "bq." prefix.
+     */
+    default Map<String, String> frameworkProperties() {
+        return subproperties(FRAMEWORK_PROPERTIES_PREFIX);
+    }
 
-	/**
-	 * Returns a map of environment variables that start with a prefix. Prefix
-	 * is stripped from the returned names.
-	 * 
-	 * @since 0.17
-	 * @param prefix
-	 *            a prefix to qualify variables with.
-	 * @return a map of environment variables that start with a prefix.
-	 */
-	Map<String, String> variables(String prefix);
+    /**
+     * Returns a value of the environment variable with a given name.
+     *
+     * @param name environment variable name.
+     * @return a value of the environment variable with a given name.
+     * @since 0.17
+     */
+    String getVariable(String name);
 
-	/**
-	 * Returns a map of all variables that start with "BQ_" prefix. Prefix is
-	 * stripped from the returned names.
-	 * 
-	 * @since 0.17
-	 * @return a map of all variables that start with "BQ_" prefix.
-	 */
-	default Map<String, String> frameworkVariables() {
-		return variables(FRAMEWORK_VARIABLES_PREFIX);
-	}
+    /**
+     * Returns a map of environment variables that start with a prefix. Prefix
+     * is stripped from the returned names.
+     *
+     * @param prefix a prefix to qualify variables with.
+     * @return a map of environment variables that start with a prefix.
+     * @since 0.17
+     */
+    Map<String, String> variables(String prefix);
+
+    /**
+     * Returns a map of all variables that start with "BQ_" prefix. Prefix is
+     * stripped from the returned names.
+     *
+     * @return a map of all variables that start with "BQ_" prefix.
+     * @since 0.17
+     * @deprecated since 0.24 as BQ_ variables are deprecated.
+     */
+    @Deprecated
+    default Map<String, String> frameworkVariables() {
+        return variables(FRAMEWORK_VARIABLES_PREFIX);
+    }
 }
