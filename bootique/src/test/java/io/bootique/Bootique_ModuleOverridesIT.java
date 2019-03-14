64e0abf97e762ea40ce0236f9fb50344361c7ae7
Andrus Adamchik <andrus@objectstyle.com>
2019-03-14 16:00:07 +0300

    Overriding multiple modules causes the submodule to be configured twice #258
    * stop using wildcard imports. This conflicts with java.lang.Module in Java 10/11

--- bootique/src/test/java/io/bootique/Bootique_ModuleOverridesIT.java
+++ bootique/src/test/java/io/bootique/Bootique_ModuleOverridesIT.java
@@ -18,7 +18,13 @@
  */
 package io.bootique;
 
-import com.google.inject.*;
+import com.google.inject.Binder;
+import com.google.inject.BindingAnnotation;
+import com.google.inject.Injector;
+import com.google.inject.Key;
+import com.google.inject.Module;
+import com.google.inject.Provides;
+import com.google.inject.Singleton;
 import io.bootique.annotation.Args;
 import org.junit.Test;
 
