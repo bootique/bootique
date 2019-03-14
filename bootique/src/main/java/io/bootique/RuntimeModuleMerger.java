2b9727ff3160d947aece4ac116f4d1ba175a63ad
Andrus Adamchik <andrus@objectstyle.com>
2019-03-14 09:46:57 +0300

    IDEA-friendly TODO format

--- bootique/src/main/java/io/bootique/RuntimeModuleMerger.java
+++ bootique/src/main/java/io/bootique/RuntimeModuleMerger.java
@@ -58,8 +58,8 @@ class RuntimeModuleMerger {
     private Collection<RuntimeModule> collectUnique(Collection<BQModule> bqModules) {
 
         // TODO: looking up modules by java type limits the use of lambdas as modules. E.g. we loaded test
-        // properties are dynamically created modules in a repeatedly called Lambda. This didn't work..
-        // So perhaps use provider name as a unique key?
+        //  properties are dynamically created modules in a repeatedly called Lambda. This didn't work..
+        //  So perhaps use provider name as a unique key?
 
         Map<Class<? extends Module>, RuntimeModule> map = new LinkedHashMap<>();
 
