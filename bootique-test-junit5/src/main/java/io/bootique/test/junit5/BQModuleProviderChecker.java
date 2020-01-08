96c7cc239e1f91525a3d899e02ae429e984feb8f
Arseni Bulatski <ancarseni@gmail.com>
2020-01-08 11:07:49 +0300

    Some refactoring

--- bootique-test-junit5/src/main/java/io/bootique/test/junit5/BQModuleProviderChecker.java
+++ bootique-test-junit5/src/main/java/io/bootique/test/junit5/BQModuleProviderChecker.java
@@ -1,3 +1,21 @@
+/*
+ * Licensed to ObjectStyle LLC under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ObjectStyle LLC licenses
+ * this file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package io.bootique.test.junit5;
 
 import io.bootique.BQModuleProvider;
@@ -20,7 +38,6 @@ import java.util.function.Consumer;
 import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
 
-import static java.util.stream.Collectors.counting;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class BQModuleProviderChecker {
@@ -56,9 +73,9 @@ public class BQModuleProviderChecker {
 
     protected void testAutoLoadable() {
 
-        Long c = matchingProviders().collect(counting());
+        long c = matchingProviders().count();
 
-        switch (c.intValue()) {
+        switch ((int) c) {
             case 0:
                 Assertions.fail("Expected provider '" + provider.getName() + "' is not found");
                 break;
@@ -93,6 +110,7 @@ public class BQModuleProviderChecker {
             });
         } catch (Exception e) {
             Assertions.fail("Fail to test metadata.");
+            throw new RuntimeException(e);
         }
     }
 
