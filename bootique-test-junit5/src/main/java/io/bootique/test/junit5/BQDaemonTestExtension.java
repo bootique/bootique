245e8d6aeb33b0d598eb490b64dd27784a51c5e4
Arseni Bulatski <ancarseni@gmail.com>
2020-01-08 11:07:49 +0300

    Some refactoring

--- bootique-test-junit5/src/main/java/io/bootique/test/junit5/BQDaemonTestExtension.java
+++ bootique-test-junit5/src/main/java/io/bootique/test/junit5/BQDaemonTestExtension.java
@@ -1,12 +1,28 @@
+/*
+ * Licensed to ObjectStyle LLC under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ObjectStyle LLC licenses
+ * this file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package io.bootique.test.junit5;
 
 import io.bootique.BQRuntime;
 import io.bootique.command.CommandOutcome;
 import io.bootique.test.junit.BQRuntimeDaemon;
 import io.bootique.test.junit.BQTestRuntimeBuilder;
-import org.junit.jupiter.api.extension.AfterAllCallback;
 import org.junit.jupiter.api.extension.AfterEachCallback;
-import org.junit.jupiter.api.extension.BeforeAllCallback;
 import org.junit.jupiter.api.extension.BeforeEachCallback;
 import org.junit.jupiter.api.extension.ExtensionContext;
 
