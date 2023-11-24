481685079857ab2785b738e674adeeb19d9a2f4f
Andrus Adamchik <andrus@objectstyle.com>
2023-11-24 13:10:49 -0500

    Deprecating BaseBQModule. It is not needed in Bootique or for user apps

--- bootique-docs/src/test/java/io/bootique/jersey/JerseyModule.java
+++ bootique-docs/src/test/java/io/bootique/jersey/JerseyModule.java
@@ -1,13 +1,17 @@
 package io.bootique.jersey;
 
-import io.bootique.di.BaseBQModule;
+import io.bootique.di.BQModule;
 import io.bootique.di.Binder;
 
 // fake JerseyModule for docs
-public class JerseyModule extends BaseBQModule {
+public class JerseyModule implements BQModule {
 
     public static Extender extend(Binder binder) {
         return new Extender();
+    }
+
+    @Override
+    public void configure(Binder binder) {
     }
 
     public static class Extender {
