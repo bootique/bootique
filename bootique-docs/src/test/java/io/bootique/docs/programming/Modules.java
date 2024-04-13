85ff451ca120e10ca540c5c2167d3f6f6289d7d9
Andrus Adamchik <andrus@objectstyle.com>
2024-04-13 09:12:18 -0400

    docs

---
+++ bootique-docs/src/test/java/io/bootique/docs/programming/Modules.java
@@ -0,0 +1,39 @@
+package io.bootique.docs.programming;
+
+import io.bootique.BQModule;
+import io.bootique.BaseModule;
+import io.bootique.Bootique;
+import io.bootique.di.Binder;
+import io.bootique.docs.programming.modules.service.MyService;
+import io.bootique.docs.programming.modules.service.MyServiceImpl;
+
+public class Modules extends BaseModule {
+
+
+    public void explicitModules() {
+
+        // tag::explicitModules[]
+        Bootique.app()
+                .module(JettyModule.class) // <1>
+                .module(binder -> binder.bind(MyService.class).to(MyServiceImpl.class)) // <2>
+                .exec();
+        // end::explicitModules[]
+    }
+
+    public void autoLoadableModules() {
+
+        // tag::autoLoadableModules[]
+        Bootique.app()
+                .autoLoadModules() // <1>
+                .module(binder -> binder.bind(MyService.class).to(MyServiceImpl.class)) // <2>
+                .exec();
+        // end::autoLoadableModules[]
+    }
+
+    static class JettyModule implements BQModule {
+        @Override
+        public void configure(Binder binder) {
+
+        }
+    }
+}
